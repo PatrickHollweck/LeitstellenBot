151aa0f07497fe749116e8571307ab5a1011c47d
PatrickHollweck <patrick-hollweck@gmx.de>
2019-10-16 16:18:34 +0200

    A Station has Vehicles

--- source/simulation/Station.ts
+++ source/simulation/Station.ts
@@ -1,7 +1,11 @@
+import { EmergencyVehicle } from "./vehicles/EmergencyVehicle";
+
 export class Station {
   public name: string;
+  public vehicles: EmergencyVehicle[];
 
-  constructor(name: string) {
+  constructor(name: string, vehicles?: EmergencyVehicle[]) {
     this.name = name;
+    this.vehicles = vehicles || [];
   }
 }
--- test/manual.ts
+++ test/manual.ts
@@ -1,3 +1,25 @@
-import { LeitstellenBot } from "../source/LeitstellenBot";
+import { Simulation } from "../source/simulation/Simulation";
 
-LeitstellenBot.start();
+import { HouseFire } from "../source/simulation/emergencies/HouseFire";
+
+import { Station } from "../source/simulation/Station";
+
+import { LF20 } from "../source/simulation/vehicles/firedept/LF20";
+import { FireLadder } from "../source/simulation/vehicles/firedept/FireLadder";
+
+const s = new Simulation();
+
+// -- Create Stations
+const fireStationPfaffenhofen = new Station("FFW Pfaffenhofen");
+
+fireStationPfaffenhofen.vehicles.push(
+  new LF20(fireStationPfaffenhofen),
+  new LF20(fireStationPfaffenhofen),
+  new FireLadder(fireStationPfaffenhofen)
+);
+
+// -- Setup Simulation
+s.stations.push(fireStationPfaffenhofen);
+s.emergencies.push(new HouseFire());
+
+s.dispatchVehicles();
